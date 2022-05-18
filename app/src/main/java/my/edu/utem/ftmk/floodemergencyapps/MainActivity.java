ba1cdcb138f24ce96b4a99a75b543f3c8eaf2184
safwan <safwanyusop220@gmail.com>
2022-05-18 13:55:11 +0800

    push

--- app/src/main/java/my/edu/utem/ftmk/floodemergencyapps/MainActivity.java
+++ app/src/main/java/my/edu/utem/ftmk/floodemergencyapps/MainActivity.java
@@ -3,12 +3,26 @@ package my.edu.utem.ftmk.floodemergencyapps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        backBtn = findViewById(R.id.backBtn);
+        //handle click go back
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
