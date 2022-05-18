b0e764b521af30f1f1ad298f6714e5bfc6e25ffc
safwan <safwanyusop220@gmail.com>
2022-05-18 14:15:09 +0800

    push

--- app/src/main/java/my/edu/utem/ftmk/floodemergencyapps/MainActivity.java
+++ app/src/main/java/my/edu/utem/ftmk/floodemergencyapps/MainActivity.java
@@ -1,15 +1,19 @@
 package my.edu.utem.ftmk.floodemergencyapps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView backBtn;
+    private CardView gyroscopeCard,floodLevelCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         backBtn = findViewById(R.id.backBtn);
+        gyroscopeCard = findViewById(R.id.gyroscopeCard);
+        floodLevelCard = findViewById(R.id.floodLevelCard);
         //handle click go back
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +30,24 @@ public class MainActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        gyroscopeCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), gyroscopeSensorActivity.class);
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(), "Directing to gyroscopeSensorActivity Menu", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        floodLevelCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FloodLevelActivity.class);
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(), "Directing to FloodLevelActivity Menu", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
