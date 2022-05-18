ab3cc674b399cb906d33747249aa99e35885ff2c
safwan <safwanyusop220@gmail.com>
2022-05-18 13:25:08 +0800

    push

---
+++ app/src/main/java/my/edu/utem/ftmk/floodemergencyapps/FloodLevelActivity.java
@@ -0,0 +1,14 @@
+package my.edu.utem.ftmk.floodemergencyapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FloodLevelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flood_level);
+    }
+}
---
+++ app/src/main/java/my/edu/utem/ftmk/floodemergencyapps/gyroscopeSensorActivity.java
@@ -0,0 +1,14 @@
+package my.edu.utem.ftmk.floodemergencyapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class gyroscopeSensorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gyroscope_sensor);
+    }
+}
